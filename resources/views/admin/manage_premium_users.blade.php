9e61f5d80dc1c4606f0726d1e12497bc4cec1704
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-02-24 21:21:56 -0700

    Revert "create premiumuser table and contact messages table in userpanel"
    This reverts commit fc6218dc0cd04e1bb785fd08fe04cbd96846947e.

--- resources/views/admin/manage_premium_users.blade.php
+++ resources/views/admin/manage_premium_users.blade.php
@@ -1,30 +1,4 @@
 @extends("layouts.adminlayout")
 @section("content")
 <h1>Manage_premium_users Page</h1>
-{{-- Table --}}
-<table class="table table-hover mt-2">
-  <thead class="green white-text">
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Username</th>
-      <th scope="col">Email</th>
-      <th scope="col">isAdmin</th>
-      <th scope="col">isPremium</th>
-      <th scope="col">Update</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-       <td>1</td>
-      <td>Zar Li</td>
-      <td>zarli@gmail.com</td>
-      <td>true</td>
-      <td>true</td>
-      <td><button class="btn btn-sm btn-success">Update</button></td>
-      <td><button class="btn btn-sm btn-danger">Delete</button></td>
-    </tr>
-
-  </tbody>
-</table>
 @endsection
