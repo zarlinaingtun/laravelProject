94a69455ead1f10f131f1583cb05e18c2c2cf4a7
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-03-21 09:44:33 -0700

    api for our project

--- resources/views/components/pagelayout.blade.php
+++ resources/views/components/pagelayout.blade.php
@@ -16,6 +16,7 @@
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css">
 {{-- custom css --}}
 <link href={{asset('css/style.css')}} rel="stylesheet" type="text/css" >
+
 </head>
 
 <body>
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,30 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts',function(){
+    $posts=Post::all();
+    return response()->json($posts);
+}); //http://localhost:4000/public/api/posts
+
+Route::get('/posts/{id}',function($id){
+    $post=Post::find($id);
+    return response()->json($post);//return with json format
+}); //http://localhost:4000/public/api/posts/14
+
+Route::post('/posts/create',function(){
+    $post=new Post();
+    $post->user_id=request('user_id');
+    $post->title=request('title');
+    $post->content=request('content');
+    $post->image=request('image');
+    $post->save();
+    return "Created Post";
+
+});//http://localhost:4000/public/api/posts/create
+
+Route::delete('/deletepost/{id}',function($id){
+    $deletepost=Post::find($id);
+    $deletepost->delete();
+    return "deleted post";
+});// http://localhost:4000/public/api/deletepost/14
